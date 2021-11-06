b3a0df2c2e7e2edcdb7bf233414817179772f094
Erna Lukac <e.lukac4@gmail.com>
2021-11-06 16:31:06 -0600

    added variables, random character grab

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -4,6 +4,84 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Click to generate a password")
 
+  let charLeg = prompt("Choose character length from 8 to 128");
+  let special = confirm ("Use special characters? \nOkay for yes, Cancel for no")  
+  let num = confirm ("Use numbers? \nOkay for yes, Cancel for no")
+  let lower = confirm ("Use lowercase characters? \nOkay for yes, Cancel for no")
+  let upper = confirm ("Use uppercase characters? \nOkay for yes, Cancel for no")
+  console.log(charLeg)
+
+  let charSpe = "";
+  let charNum = "";
+  let charLow = "";
+  let charUpp = "";
+
+  if (special === true) {
+    charSpe="!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+
+  if (num === true) {
+    charNum="1234567890";
+  }
+
+  if (lower === true) {
+    charLow="abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (upper === true) {
+    charUpp="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  let charMin = 2;
+  let randomCharSpe = "";
+  let randomCharNum = "";
+  let randomCharLow = "";
+  let randomCharUpp = "";
+
+  if (special === true) {
+    for (let i=0; i < charMin; i++) {
+      randomCharSpe += charSpe.charAt(Math.floor(Math.random() * charSpe.length));
+    }
+  }
+
+  if (num === true) {
+    for (let i=0; i < charMin; i++) {
+      randomCharNum += charNum.charAt(Math.floor(Math.random() * charNum.length));
+    }
+  }
+
+  if (lower === true) {
+    for (let i=0; i < charMin; i++) {
+      randomCharLow += charLow.charAt(Math.floor(Math.random() * charLow.length));
+    }
+  }
+
+  if (upper === true) {
+    for (let i=0; i < charMin; i++) {
+      randomCharUpp += charUpp.charAt(Math.floor(Math.random() * charUpp.length));
+    }
+  }
+
+  let randomCharAll=randomCharSpe+randomCharNum+randomCharLow+randomCharUpp;
+  let loop = charLeg - randomCharAll.length; 
+  let charAll=charSpe+charNum+charLow+charUpp;
+
+  let genPass = "";
+
+  for (let i=0; i< loop; i++) {
+    console.log(i)
+    genPass += charAll.charAt(Math.floor(Math.random() * charAll.length));
+    console.log(genPass)
+  }
+
+  let pass=randomCharAll+genPass;
+  return pass;
+
+  // return genPass;
+
+  
+
+
 // 1. prompt the user for the password criteria
 //    a. password length 8-128
 //    b. lowercase, uppercase, numbers, special characters
